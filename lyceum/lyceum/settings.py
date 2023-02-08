b26cff676890d84c9eba2e8813f437e07b090bde
Andrew <andrey777-novikov777@yandex.ru>
2023-02-08 21:37:24 +0300

    Обновление под стандарты flake8
    +Обновлено описание,
    +Создан requirements для разработки. Пока что там только инструмент Black

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p@3pt0u#i(y47f4bgd&zgkq6*^-!%q(l*v7cf6o=&3cbd=v8ko"
+SECRET_KEY = (
+    "django-insecure-p@3pt0u#i(y47f4bgd&zgkq6*^-!%q(l*v7cf6o=&3cbd=v8ko"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,9 +33,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "catalog.apps.CatalogConfig",
-    "homepage.apps.HomepageConfig",
-    "about.apps.AboutConfig",
+    # Пока что неиспользованные, но уже зарегистрированные приложения.
+    # "catalog.apps.CatalogConfig",
+    # "homepage.apps.HomepageConfig",
+    # "about.apps.AboutConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -89,16 +92,23 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation.MinimumLengthValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation.CommonPasswordValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation.NumericPasswordValidator"
+        ),
     },
 ]
 
